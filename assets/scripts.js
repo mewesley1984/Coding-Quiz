74fef94fa7eeaa98572f57508ec6c516c7ace61d
Matt  Wesley <mattwesley@Matts-MacBook-Air.local>
2023-04-01 14:27:04 -0400

    added js

--- assets/scripts.js
+++ assets/scripts.js
@@ -170,6 +170,9 @@ function saveScore() {
     users.push(userInfo)
 
     localStorage.setItem("users", JSON.stringify(users))
+    endGameElement.className = "hide"
+    showScores()
+    
 }
 
 function showScores() {
