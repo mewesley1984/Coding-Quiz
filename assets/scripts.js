7e550bc7db2adfc912a558c1198ea9eca8c1d0bc
Matt  Wesley <mattwesley@Matts-MacBook-Air.local>
2023-03-31 20:49:24 -0400

    setup local storage, fixed high score, fixed other bugs

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,121 +1,155 @@
 
 // button to start quiz
-    // user clicks button
-        // addeventlistner
+// user clicks button[]
+// addeventlistner
 // timer starts
-    // countdown, integer reduced by 1 until 0
+// countdown, integer reduced by 1 until 0
 // user gets question
-    // DOM Manipulation (writing HTML with Javascript)
+// DOM Manipulation (writing HTML with Javascript)
 // user gets 4 answers
 // user clicks answer
-    // addeventlistner 
+// addeventlistner 
 // if they get it right get points / if they get wrong minus time
-    // if/else statements
+// if/else statements
 // user gets next question
-    // DOM Manipulation (writing HTML with Javascript)
+// DOM Manipulation (writing HTML with Javascript)
 // happen 5 times
-    // one iteration per answer (button click)
+// one iteration per answer (button click)
 // user answers last question
-    // if/else statements
+// if/else statements
 // when timer stops or questions answered the game is over
-    // already done
+// already done
 // users can add initials to their score
-    // input element for initials, submit button
+// input element for initials, submit button
 
 
 var startButton = document.querySelector(".start-game");
-var questionContainerEl =document.getElementById("question-container");
+var questionContainerElement = document.querySelector("#question-container");
 var timerElement = document.querySelector(".timer");
-var answerButtonsElement = document.getElementById("answer-buttons");
-var questionElement = document.getElementById('question');
-var timer;
-var timerCount;
-var currentQuestionIndex;
-var userNameEl=document.getElementById("name")
+var answerButtonsElement = document.querySelector("#answer-buttons");
+var questionElement = document.querySelector('#question');
+var quizElement = document.querySelector('#quiz-container');
+var welcomeElement = document.querySelector('#welcome-container');
+var saveScoreButton = document.querySelector('#save-score')
+var endGameElement = document.querySelector('#end-game');
+var highScoreLink = document.querySelector('.high-score a');
+var highScoresElement = document.querySelector('#high-scores');
+var finalScoreElement = document.querySelector('#final-score')
 
-startButton.addEventListener("click",startGame);
-var question = [
-    {
-        question: "Commonly used data types DO NOT include:",
-        answer: [
-            {text: "alerts", correct: true},
-            {text: "strings", correct: false},
-            {text: "booleans", correct: false},
-            {text: "numbers", correct: false}]
-    },
-    {
-        question: "The condition in an if/else statment is enclosed within _____.",
-        answer: [
-            {text: "parenthesis", correct: true},
-            {text: "quotes", correct: false},
-            {text: "curly brackets", correct: false},
-            {text: "commas", correct: false}]
-   
-    },
-    {
-        question: "Arrays in JavaScript can be used to store ______.",
-        answer: [
-            {text: "numbers & strings", correct: false},
-            {text: "other arrays", correct: false},
-            {text: "booleans", correct: false},
-            {text: "all of the above", correct: true}]
-    },
-    {
-        question: "String values must be enclosed within ____ when being assigned to variables.",
-        answer: [
-            {text: "commas", correct: false},
-            {text: "curly brackets", correct: false},
-            {text: "quotes", correct: true},
-            {text: "parenthesis", correct: false}]
-    },
-    {
-        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        answer: [
-            {text: "JavaScript", correct: false},
-            {text: "console.log", correct: true},
-            {text: "terminal/bash", correct: false},
-            {text: "for-loops", correct: false}]
-    },
+const gameState = {
+    currentQuestion: 0,
+    score: 0,
+    secondsRemaining: 75
+}
+var userNameElement = document.querySelector("#name");
+var isLocalStorageEmpty = localStorage.getItem("users") === null
+if(isLocalStorageEmpty) {
+    localStorage.setItem("users", "[]")
+}
 
-]
+startButton.addEventListener("click", startGame);
+saveScoreButton.addEventListener("click", saveScore);
+highScoreLink.addEventListener("click", showScores)
 
 function startGame() {
-    timerCount= 75;
-   document.getElementById('page-1').style.display="none";
-   document.getElementById('page-2').style.display='block';
-    currentQuestionIndex = 0 
+    // TODO: reset game state 
+    welcomeElement.className = "hide"
+    quizElement.className = "show"
     startTimer()
     showQuestion()
 }
 
+function showNextQuestion() {
+    gameState.currentQuestion++
+    showQuestion()
+}
+
 function showQuestion() {
-// this identifies the element in the html and places the question in it.
-    questionElement.innerText = question[currentQuestionIndex].question
-    for (var i = 0; i < question[currentQuestionIndex].answer.length; i++){
-// this identifies the answer-button elements children and places answer options in it.
-        answerButtonsElement.children[i].textContent = question[currentQuestionIndex].answer[i].text;
-     answerButtonsElement.children[i].value=question[currentQuestionIndex].answer[i].correct 
-     answerButtonsElement.children[i].onclick=checkAnswer  
+    const questions = [
+        {
+            question: "Commonly used data types DO NOT include:",
+            answer: [
+                { text: "alerts", isCorrect: true },
+                { text: "strings", isCorrect: false },
+                { text: "booleans", isCorrect: false },
+                { text: "numbers", isCorrect: false }]
+        },
+        {
+            question: "The condition in an if/else statment is enclosed within _____.",
+            answer: [
+                { text: "parenthesis", isCorrect: true },
+                { text: "quotes", isCorrect: false },
+                { text: "curly brackets", isCorrect: false },
+                { text: "commas", isCorrect: false }]
+    
+        },
+        {
+            question: "Arrays in JavaScript can be used to store ______.",
+            answer: [
+                { text: "numbers & strings", isCorrect: false },
+                { text: "other arrays", isCorrect: false },
+                { text: "booleans", isCorrect: false },
+                { text: "all of the above", isCorrect: true }]
+        },
+        {
+            question: "String values must be enclosed within ____ when being assigned to variables.",
+            answer: [
+                { text: "commas", isCorrect: false },
+                { text: "curly brackets", isCorrect: false },
+                { text: "quotes", isCorrect: true },
+                { text: "parenthesis", isCorrect: false }]
+        },
+        {
+            question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+            answer: [
+                { text: "JavaScript", isCorrect: false },
+                { text: "console.log", isCorrect: true },
+                { text: "terminal/bash", isCorrect: false },
+                { text: "for-loops", isCorrect: false }]
+        },
+    
+    ],
+    question = questions[gameState.currentQuestion]
+    // this identifies the element in the html and places the question in it.
+
+    if(gameState.currentQuestion + 1 >= questions.length) {
+        questionContainerElement.className = 'hide'
+        endGameElement.className = 'show'
+        finalScoreElement.innerText = gameState.score
+        return
     }
-   
+    questionElement.innerText = question.question
+    for (var i = 0; i < question.answer.length; i++) {
+        // this identifies the answer-button elements children and places answer options in it.
+        answerButtonsElement.children[i].textContent = question.answer[i].text;
+        answerButtonsElement.children[i].value = question.answer[i].isCorrect
+        answerButtonsElement.children[i].onclick = checkAnswer
+    }
+
 }
 function checkAnswer(event) {
     console.log(event.target.value)
-    if (event.target.value === "false"){
-        alert("Wrong");
+    if (event.target.value === 'true') {
+     gameState.score++;
+     
+        document.querySelector("#status").innerHTML = `Correct! score = ${gameState.score}`
     }
-    else alert("Correct!");
-
-    showQuestion()
+    else {
+        gameState.secondsRemaining = gameState.secondsRemaining - 10;
+        document.querySelector("#status").innerHTML = "Wrong!"
+    }
+    
+    showNextQuestion()
 }
 
 
 function startTimer() {
-    timer = setInterval(function() {
-        timerCount--;
-        timerElement.textContent = timerCount;
-        if (timerCount === 0) {
-            timer.textcontent = "Times Up!";
+    const timerId = setInterval(function () {
+        gameState.secondsRemaining--;
+        timerElement.textContent = gameState.secondsRemaining;
+        if (gameState.secondsRemaining <= 0) {
+            clearInterval(timerId)
+            timerElement.innerHTML = "Game Over! ¯\_(ツ)_/¯ ";
         }
     }, 1000);
 }
@@ -123,20 +157,22 @@ function startTimer() {
 function getScore() {
     var storedScore = localStorage.getItem("score")
 }
-    // use json stringify here
 
-function storehighScore(){
-  var nameUser= userNameEl.value();
+function saveScore() {
+    var nameUser = userNameElement.value;
     var userInfo = {
-        userName: nameUser,
-        userScore: secondCount,
+        name: nameUser,
+        score: gameState.score,
     };
-   var highScorer = JSON.stringify(userInfo);
+    var users = JSON.parse(localStorage.getItem("users"))
 
-   localStorage.setItem("score",highScorer);
-    //  get userscore and name
+    users.push(userInfo)
 
-    // make a user object with user score and name
-    // json stringify object into local storage (items in storage need to be strings)
+    localStorage.setItem("users", JSON.stringify(users))
+}
+
+function showScores() {
+    var users = JSON.parse(localStorage.getItem("users"))
+    highScoresElement.innerHTML = JSON.stringify(users)
 }
 
